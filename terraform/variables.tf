ee8dc7dfbf014eda89ea509eefe5b1f20a274c0c
Fares <faresraboudi308@gmail.com>
2024-10-26 11:02:45 +0100

    configure terraform files to work with aws

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,51 +1,54 @@
 variable "aws_region" {
   description = "La région AWS"
   type        = string
-  default     = "us-east-1"  # Région mise à jour
+  default     = "us-east-1"
 }
 
 variable "aws_access_key" {
   description = "AWS Access Key ID"
   type        = string
+  sensitive = true
 }
 
 variable "aws_secret_key" {
   description = "AWS Secret Access Key"
   type        = string
+  sensitive = true
 }
 
 variable "aws_session_token" {
   description = "AWS Session Token"
   type        = string
+  sensitive = true
 }
 
 
 variable "cluster_name" {
   description = "Nom du cluster EKS"
   type        = string
-  default     = "mykubernetes"  # Nom du cluster mis à jour
+  default     = "terraform-cluster"  # Nom du cluster mis à jour
 }
 
 variable "subnet_ids" {
   description = "IDs des sous-réseaux"
   type        = list(string)
-  default     = ["subnet-075e0dd45da90bdb8", "subnet-0114974e6dbd8217e"]  # Valeurs par défaut
+  default     = ["subnet-00b51509a69be2fa0", "subnet-0860a63499c7dced5"]  # Valeurs par défaut
 }
 
 variable "role_arn" {
   description = "ARN du rôle IAM pour EKS"
   type        = string
-  default     = "arn:aws:iam::744983671605:role/LabRole"  # Valeur par défaut
+  default     = "arn:aws:iam::374547093317:role/LabRole"  # Valeur par défaut
 }
 
 variable "vpc_id" {
   description = "L'ID du VPC pour le cluster EKS"
   type        = string
-  default     = "vpc-04c984733bc5b455e"  # Remplacez par votre ID de VPC réel
+  default     = "vpc-04b1bb3cc595c1cc2"  # Remplacez par votre ID de VPC réel
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"  # Modifiez-le selon vos besoins
+  default     = "172.31.0.0/16"  # Modifiez-le selon vos besoins
 }
